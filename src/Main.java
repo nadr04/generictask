bcec47e53226a3af713c7889a8cac50ea37a71a6
nadezda <nadezda@mail.com>
2025-01-09 18:39:38 +0200

    add task1 solution

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Integer[] skaiciuArray = {1, 2, 3};
+        String[] zodziuArray = {"Hello", "World"};
+
+        printArray(skaiciuArray);
+        printArray(zodziuArray);
+
+    }
+
+
+    public static <T> void printArray(T[] allTypesArray) {
+        for (T arrayElements : allTypesArray) {
+            System.out.println(arrayElements);
+        }
+
     }
 }
